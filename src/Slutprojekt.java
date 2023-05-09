b7a85e46af0cabe00a3697435f551ae671e14451
NonsensicalQuagga <94972468+NonsensicalQuagga@users.noreply.github.com>
2023-05-09 15:08:44 +0200

    added code that creates a window

--- src/Slutprojekt.java
+++ src/Slutprojekt.java
@@ -1,6 +1,135 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferStrategy;
+
+public class Slutprojekt extends Canvas implements Runnable{
+    private BufferStrategy bs;
+
+    private boolean running = false;
+    private Thread thread;
+
+    private int width = Integer.parseInt(JOptionPane.showInputDialog("width"));
+    private int height = Integer.parseInt(JOptionPane.showInputDialog("height"));
+
+    public Slutprojekt() {
+        setSize(width,height);
+        JFrame frame = new JFrame();
+        frame.add(this);
+        this.addMouseMotionListener(new MyMouseMotionListener());
+        this.addMouseListener(new MyMouseListener());
+        requestFocus();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void render() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        // Rita ut den nya bilden
+        draw(g);
+
+        g.dispose();
+        bs.show();
+    }
+
+    public void draw(Graphics g) {
+        g.clearRect(0,0,getWidth(),getHeight());
+        //add function for drawing the pixels
+    }
+
+    private void update() {
+        // may be redundant
+
+    }
+
+    public static void main(String[] args) {
+        Slutprojekt minGrafik = new Slutprojekt();
+        minGrafik.start();
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        double ns = 1000000000.0 / 25.0;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            while(delta >= 1) {
+                // Uppdatera koordinaterna
+                update();
+                // Rita ut bilden med updaterad data
+                render();
+                delta--;
+            }
+        }
+        stop();
+    }
+
+    public class MyMouseMotionListener implements MouseMotionListener {
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
 
 
+        }
+    }
 
+    public class MyMouseListener implements MouseListener {
 
-public class Slutprojekt {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
 }
